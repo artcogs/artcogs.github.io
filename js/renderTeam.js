61b31cd76c6ebd2293ede2e466f18b3befdd7cf1
artcogs <chairiq2@gmail.com>
2026-04-21 14:23:23 +0300

    update

--- js/renderTeam.js
+++ js/renderTeam.js
@@ -66,8 +66,8 @@ function renderPeople(list, containerId, size = "normal") {
       .join("");
 
     const card = `
-      <div class="${config.col} mb-3">
-        <div class="card text-center h-100 shadow-sm ${config.card}">
+      <div>
+        <div class="card text-center shadow-sm ${config.card}">
           <div class="card-body d-flex flex-column">
 
             <img src="${person.img}" 
