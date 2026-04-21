d387867065802a9f50a0b42cf9b00d94adb0684b
artcogs <chairiq2@gmail.com>
2026-04-21 12:01:20 +0300

    update

---
+++ js/team.js
@@ -0,0 +1,193 @@
+const teamData = {
+
+  team: [
+    {
+      id: "chairikiourt",
+      name: "Dr. Chairi Kiourt",
+      role: ["Principal Researcher", "Artificial Intelligence", "Head of ArtCogS Group"],
+      email: "chairiq(at)atheanrc.gr",
+      img: "imgs/profile/Kiourt.Chairi.png",
+      links: {
+        linkedin: "https://www.linkedin.com/in/chairiq",
+        scholar: "https://scholar.google.gr/citations?user=jR0HOoIAAAAJ&hl=en",
+        orcid: "https://orcid.org/0000-0001-8501-8899",
+        scopus: "https://www.scopus.com/authid/detail.uri?authorId=55669286500",
+        webpage: "https://chairiq.github.io/"
+      }
+    },
+    {
+      id: "dimitriskalles",
+      name: "Prof. Dimitris Kalles",
+      role: ["Senior Researcher", "Artificial Intelligence"],
+      email: "kalles(at)eap.gr",
+      img: "imgs/profile/kalles.jpg",
+      links: {
+        linkedin: "https://www.linkedin.com/in/dimitris-kalles-4972584/",
+        scholar: "https://scholar.google.gr/citations?user=mfu6U_MAAAAJ&hl=en",
+        orcid: "https://orcid.org/0000-0003-0364-5966",
+        webpage: "https://sites.google.com/site/dimitriskalles/"
+      }
+    },
+    {
+      id: "vassilisevangelidis",
+      name: "Dr. Vassilis Evangelidis",
+      role: ["Senior Researcher", "Digital Archaeology"],
+      email: "vevangelidis(at)athenarc.gr",
+      img: "imgs/profile/vassilis.evangelidis.jpg",
+      links: {
+        linkedin: "https://www.linkedin.com/in/vassilis-evangelidis-32a5a794/",
+        scholar: "https://scholar.google.com/citations?user=wHHafzsAAAAJ&hl=en",
+        orcid: "https://orcid.org/0000-0003-1365-99319",
+        academia: "https://athena-innovation.academia.edu/VasilisEvangelidis"
+      }
+    },
+    {
+      id: "vassiliszafeiropoulos",
+      name: "Dr. Vassilis Zafeiropoulos",
+      role: ["Junior Researcher", "ML and XR Technologies"],
+      email: "v.zafeiropoulos(at)athenarc.gr",
+      img: "imgs/profile/VasilisZafeiropoulos.png",
+      links: {
+        linkedin: "https://www.linkedin.com/in/vasilisgzafeiropoulos/",
+        scholar: "https://scholar.google.com/citations?user=s5Kzsi8AAAAJ&hl=en",
+        orcid: "https://orcid.org/my-orcid?orcid=0000-0003-0120-0488",
+        researchgate: "https://www.researchgate.net/profile/Vasilis_Zafeiropoulos"
+      }
+    },
+    {
+      id: "vassilisperifanis",
+      name: "Dr. Vassilis Perifanis",
+      role: ["Junior Researcher", "ML Technologies"],
+      email: "vperifan(at)athenarc.gr",
+      img: "imgs/profile/perifanis.jpg",
+      links: {
+        linkedin: "https://www.linkedin.com/in/vasileios-perifanis/",
+        scholar: "https://scholar.google.com/citations?user=V1t6u_YAAAAJ&hl=el"
+      }
+    },
+    {
+      id: "helenatheodoropoulou",
+      name: "Helena G. Theodoropoulou",
+      role: ["PhD Candidate", "AI, XR and Creativity"],
+      email: "hetheod(at)athenarc.gr",
+      img: "imgs/profile/helen.jpg",
+      links: {
+        linkedin: "https://www.linkedin.com/in/elenatheodoropoulou/",
+        scholar: "https://scholar.google.com/citations?user=NjFk0rEAAAAJ&hl=en",
+        orcid: "https://orcid.org/0000-0002-5896-0739",
+        academia: "https://independent.academia.edu/ElenaTheodoropoulou"
+      }
+    },
+    {
+      id: "zoilygizou",
+      name: "Zoi Lygizou",
+      role: ["PhD Candidate", "RL-Agents Technologies"],
+      img: "imgs/profile/zoi.jpg",
+      links: {
+        linkedin: "https://www.linkedin.com/in/",
+        orcid: "https://orcid.org/0000-0001-8414-7963./"
+      }
+    },
+    {
+      id: "michaliszervas",
+      name: "Michalis Zervas",
+      role: ["ML Engineer"],
+      email: "michalis.zervas(at)athenarc.gr",
+      img: "imgs/profile/zervas.jpeg",
+      links: {
+        linkedin: "https://www.linkedin.com/in/michail-zervas/",
+        scholar: "https://scholar.google.com/",
+        orcid: "https://orcid.org/",
+        academia: "https://independent.academia.edu/u"
+      }
+    },
+    {
+      id: "gianniszikos",
+      name: "Giannis Zikos",
+      role: ["Software & ML Engineer"],
+      email: "giannis.zikos(AT)athenarc.gr",
+      img: "imgs/profile/person-place-holder.jpg",
+      links: {
+        linkedin: "https://www.linkedin.com/in/ioannis-zikos-84a868255/"
+      }
+    }
+  ],
+
+  collaborators: [
+    {
+      id: "konstantinoschatzilygeroudis",
+      name: "Dr. Konstantinos Chatzilygeroudis",
+      role: ["Assistant Professor"],
+      img: "imgs/ibrics/costas.jpg",
+      links: {
+        webpage: "http://costashatz.github.io/"
+      }
+    },
+    {
+      id: "dimitriskastaniotis",
+      name: "Dr. Dimitris Kastaniotis",
+      role: ["Machine Learning & Computer Vision"],
+      img: "imgs/ibrics/kastaniotis.jpeg",
+      links: {
+        webpage: "https://scholar.google.gr/citations?user=V2r7hKEAAAAJ&hl=en"
+      }
+    },
+    {
+      id: "lamprosprintzios",
+      name: "Lampros Printzios",
+      role: ["Machine Learning & Computer Vision"],
+      img: "imgs/ibrics/printzios.png",
+      links: {
+        webpage: "#"
+      }
+    },
+    {
+      id: "konstantinosasimakopoulos",
+      name: "Konstantinos Asimakopoulos",
+      role: ["Physics Informed Reinforcement Learning"],
+      img: "imgs/ibrics/asimakopoulos.jpg",
+      links: {
+        webpage: "#"
+      }
+    },
+    {
+      id: "panossyriopoulos",
+      name: "Panos Syriopoulos",
+      role: ["PhD Student, University of Patras (Department of Mathematics)"],
+      img: "imgs/profile/person-place-holder.jpg",
+      links: {
+        webpage: "https://www.researchgate.net/profile/Panos-Syriopoulos"
+      }
+    },
+    {
+      id: "aristeidisandroutsopoulos",
+      name: "Aristeidis Androutsopoulos",
+      role: ["Robotic Skill Discovery via Manifold Techniques"],
+      img: "imgs/ibrics/aa.jpg",
+      links: {
+        webpage: "#"
+      }
+    }
+  ],
+
+  former: [
+    {
+      id: "evangelostsiatsianas",
+      name: "Evangelos Tsiatsianas",
+      role: ["Kinodynamic Contact Planning"],
+      img: "imgs/ibrics/tsiatsianas.jpeg",
+      links: {
+        webpage: "https://lar.upatras.gr/projects/ibrics.html"
+      }
+    },
+    {
+      id: "alexandrosntagkas",
+      name: "Alexandros Ntagkas",
+      role: ["PhD Student, Oxford Robotics Institute"],
+      img: "imgs/ibrics/ntagkas.jpeg",
+      links: {
+        webpage: "https://lar.upatras.gr/projects/ibrics.html"
+      }
+    }
+  ]
+};
